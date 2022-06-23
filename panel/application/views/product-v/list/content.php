e4ecc401bd49afa116a8d74acbf869ba1bbebec4
bdrhnkr <bedirhankr34@gmail.com>
2022-06-23 16:34:00 +0300

    .

--- panel/application/views/product-v/list/content.php
+++ panel/application/views/product-v/list/content.php
@@ -1,1 +1,40 @@
-Ürün listesi content
+product-v -> content.php
+<div class="row">
+	<div class="col-md-12">
+		<h4 class="m-b-lg">Ürün Listesi</h4>
+	</div><!-- END column -->
+	<div class="col-md-12">
+		<div class="widget p-lg">
+		
+			<table class="table table-striped table-hover">
+
+				<thead>
+					<th>#id</th>
+					<th>url</th>
+					<th>Başlık</th>
+					<th>Açıklama</th>
+					<th>Durumu</th>
+					<th>İşlem</th>
+				</theah>
+
+				<tbody>
+					<tr>
+						<td>#1</td>
+						<td>monitor</td>
+						<td>Monitör</td>
+						<td>360 derece dönebilen monitör</td>
+						<td>1/0</td>
+						<td>
+						    <button type="button" class="btn btn-sm btn-danger btn-outline"><i class="fa fa-trash"></i> Sil</button>
+						    <button type="button" class="btn btn-sm btn-warning btn-outline"><i class="fa fa-pencil-square-o"></i> Düzenle</button>
+						</td>
+					</tr>
+					
+				</tbody>
+
+			</table>
+		</div><!-- .widget -->
+	</div><!-- END column -->
+
+
+</div>
